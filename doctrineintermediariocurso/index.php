94468a04916706dfda4ecaa2b47bc139e4c03cb9
Marco Antônio Dutra Teixeira <madtbr@gmail.com>
2019-01-08 08:30:06 -0200

    repo limpo

---
+++ doctrineintermediariocurso/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ .'/vendor/autoload.php';
+require_once __DIR__ .'/src/doctrine.php';
+$entityManager  = getEntityManager();
+$category       = new \PMSG\Entity\Category();
+$category->setName("Minha primeira categoria");
+$entityManager->persist($category);
+$entityManager->flush(); 
+
+/*
+//$user = $entityManager->find('SON\\Entity\\User',1);
+$user = new \SON\Entity\User();
+$user->setName("Luiz Carlos")
+    ->setEmail('luiz@schoolofnet.com');
+$address = new \SON\Entity\Address();
+$address->setStreet('Rua X')
+    ->setCity('Cidade Y');
+$user->setAddress($address);
+/** @var \SON\Entity\UserRepository $repository 
+$repository = $entityManager->getRepository('SON\\Entity\\User');
+$repository->create($user);
+dump($repository->find(1));
+*/
---
+++ doctrineintermediariocurso/src/Entity/Category.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace PMSG\Entity;
+
+/**
+ * @Entity
+ * @Table(name="categories")
+ */
+class Category{
+    
+    /**
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @Column(type="string")
+     */
+    private $name;
+    
+    /**
+     * @Column(type="string")
+     */
+    private $exemplo;
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
